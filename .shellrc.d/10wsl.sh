be70170e5d444b059b21741a43a20064df04eda3
Jonathan Bowman <jbowmancargas@users.noreply.github.com>
2022-09-07 15:52:20 -0400

    cleaned up wsl launch script

--- .shellrc.d/10wsl.sh
+++ .shellrc.d/10wsl.sh
@@ -10,18 +10,30 @@ if [[ -z "$XDG_RUNTIME_DIR" ]]; then
   fi
 fi
 
-PROMPT_COMMAND=${PROMPT_COMMAND:+"$PROMPT_COMMAND; "}'printf "\e]9;9;%s\e\\" "$(wslpath -w "$PWD")"'
-
-WINHOSTS="/mnt/c/Windows/System32/drivers/etc/hosts"
-ip=$(ip addr show label eth0 | rg -ow 'inet ([^/]+)' -r '$1')
-
-if ! rg -q --crlf "$ip" /mnt/c/Windows/System32/drivers/etc/hosts; then
-	tmpfile=$(mktemp)
-	rg --crlf -v '^\d{1,3}(\.\d{1,3}){3}\s+wsl' "$WINHOSTS" > "$tmpfile"
-	printf "$ip    wsl\r\n" >> "$tmpfile"
-	$HOME/win/scoop/shims/sudo.exe pwsh -Command 'mv -Force '$(wslpath -w $tmpfile)' C:\Windows\System32\drivers\etc\hosts'
-	unset tmpfile
+export DOCKER_DISTRO="fedora"
+DOCKER_DIR=/mnt/wsl/shared-docker
+DOCKER_SOCK="$DOCKER_DIR/docker.sock"
+export DOCKER_HOST="unix://$DOCKER_SOCK"
+if [ ! -S "$DOCKER_SOCK" ]; then
+    mkdir -pm o=,ug=rwx "$DOCKER_DIR"
+    chgrp docker "$DOCKER_DIR"
+		cd /mnt/c
+    /mnt/c/Windows/System32/wsl.exe -d "$DOCKER_DISTRO" sh -c "nohup sudo -b dockerd < /dev/null > $DOCKER_DIR/dockerd.log 2>&1"
+		cd "$OLDPWD"
 fi
 
-unset ip
+PROMPT_COMMAND=${PROMPT_COMMAND:+"$PROMPT_COMMAND; "}'printf "\e]9;9;%s\e\\" "$(wslpath -w "$PWD")"'
+
+# WINHOSTS="/mnt/c/Windows/System32/drivers/etc/hosts"
+# ip=$(ip addr show label eth0 | rg -ow 'inet ([^/]+)' -r '$1')
+# 
+# if ! rg -q --crlf "$ip" /mnt/c/Windows/System32/drivers/etc/hosts; then
+# 	tmpfile=$(mktemp)
+# 	rg --crlf -v '^\d{1,3}(\.\d{1,3}){3}\s+wsl' "$WINHOSTS" > "$tmpfile"
+# 	printf "$ip    wsl\r\n" >> "$tmpfile"
+# 	$HOME/win/scoop/shims/sudo.exe pwsh -Command 'mv -Force '$(wslpath -w $tmpfile)' C:\Windows\System32\drivers\etc\hosts'
+# 	unset tmpfile
+# fi
+# 
+# unset ip
 
