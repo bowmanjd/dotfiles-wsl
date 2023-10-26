5a658b2b8bddaef06b8e86d23e3b144ff47fb634
Jonathan Bowman <jbowman@cargas.com>
2023-10-26 10:12:22 -0400

    fix: adjusted for go-sqlcmd

--- .config/db.sh
+++ .config/db.sh
@@ -3,3 +3,7 @@
 export SQLCMDSERVER="$SQLSERVER,$SQLPORT"
 
 export CUSTOMER=$(sqlcmd -C -l 1 -W -h -1 -Q "SET NOCOUNT ON; SELECT TOP 1 Name FROM bCompany" 2> /dev/null)
+
+if [ "$(echo $CUSTOMER | cut -d' ' -f 1-3)" = "unable to open" ]; then
+	export CUSTOMER=""
+fi
